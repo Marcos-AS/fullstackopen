797d420bca41fc0d76b55b9435d6ba3e4a4a7136
Marcos-AS <marcossantangelo1@gmail.com>
2024-03-15 14:38:01 -0300

    hasta 1.5 terminado

---
+++ part1/src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+//renderiza el componente App dentro del elemento div en index.html
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
